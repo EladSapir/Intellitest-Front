c451a5610091eba9d43034df243dc5d53f200b88
Elad Sapir <elad.1999.1999@gmail.com>
2024-06-19 01:12:35 +0300

    image container

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import MainSection from './Components/MainSection';
 import Features from './Components/Features';
 import ImageSection from './Components/ImageSection';
 import Footer from './Components/Footer';
-import Login from './Components/Login';
 import './App.css';
 import GlobalStyle from './GlobalStyle';
+import bgImg from './Images/BG Image.png';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Header />
-      <MainSection />
-      <Features />
-      <ImageSection />
-      <Footer />
+      <div className="content-container">
+        <div className="background-image">
+          <img src={bgImg} alt="bgImage" className="bg-img" />
+        </div>
+        <MainSection />
+        <Features />
+        <ImageSection />
+        <Footer />
+      </div>
     </div>
   );
 }
--- src/Components/ImageSection.js
+++ src/Components/ImageSection.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import think from '../Images/think.png'
 import './ImageSection.css';
 
 const ImageSection = () => (
   <div className="image-container">
-    <img src="path/to/your/image.png" alt="Empower AI Innovations" className="image" />
+    <div className="text-content">
+      <h3>Empower Your AI Innovations: Unleash the Power of Testing!</h3>
+      <h3>Welcome to AI Testing Empowerment</h3>
+      <p>
+        Welcome to a world where AI testing is not just a necessity but a catalyst for innovation! Our platform empowers individuals and organizations to validate and optimize AI-based systems with confidence and precision. Whether you're a seasoned professional or a curious newcomer, our intuitive tools and comprehensive testing methodologies are here to support your journey towards AI excellence.
+      </p>
+      <a href="#top" className="start-button">Start Testing Today!</a>
+    </div>
+    <img src={think} alt="Empower AI Innovations" className="image" />
   </div>
 );
 

a3818cb1d3f859159817f45bd466b0da90c3ab5b
Elad Sapir <elad.1999.1999@gmail.com>
2024-06-25 15:28:07 +0300

    fixing css duplicate

--- src/LandingPage-Components/ImageSection.js
+++ src/LandingPage-Components/ImageSection.js
@@ -3,16 +3,16 @@ import think from '../Images/think.png'
 import './ImageSection.css';
 
 const ImageSection = () => (
-  <div className="image-container">
-    <div className="text-content">
-      <h3>Empower Your AI Innovations: Unleash the Power of Testing!</h3>
-      <h4>Welcome to AI Testing Empowerment</h4>
-      <p>
+  <div className="image-image-container">
+    <div className="image-text-content">
+      <h3 className='image-h3'>Empower Your AI Innovations: Unleash the Power of Testing!</h3>
+      <h4 className='image-h4'>Welcome to AI Testing Empowerment</h4>
+      <p className='image-p'>
         Welcome to a world where AI testing is not just a necessity but a catalyst for innovation! Our platform empowers individuals and organizations to validate and optimize AI-based systems with confidence and precision. Whether you're a seasoned professional or a curious newcomer, our intuitive tools and comprehensive testing methodologies are here to support your journey towards AI excellence.
       </p>
-      <a href="#top" className="start-button">Start Testing Today!</a>
+      <a href="#top" className="image-start-button">Start Testing Today!</a>
     </div>
-    <img src={think} alt="Empower AI Innovations" className="image" />
+    <img src={think} alt="Empower AI Innovations" className="image-image" />
   </div>
 );
 

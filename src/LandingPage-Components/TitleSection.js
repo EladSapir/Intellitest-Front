a7503ec5bc94102c48245e970c38536a81bbe7fe
Elad Sapir <elad.1999.1999@gmail.com>
2024-06-25 15:16:16 +0300

    fix title css

--- src/LandingPage-Components/TitleSection.js
+++ src/LandingPage-Components/TitleSection.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './TitleSection.css';
 
 const TitleSection = () => (
-  <div className="text-section">
-    <h1 className="title">
+  <div className="text-section-intitle">
+    <h1 className="title-intitle">
       Revolutionize 
-      <span className="title-second-line">AI Accuracy</span>
+      <span className="title-second-line-intitle">AI Accuracy</span>
     </h1>
-    <h2 className="subtitle">Join the Future Today!</h2>
+    <h2 className="subtitle-intitle">Join the Future Today!</h2>
   </div>
 );
 

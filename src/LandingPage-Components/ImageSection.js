e5c858e45572b8b953524f99ad8fa27bc47f3dd0
Elad Sapir <elad.1999.1999@gmail.com>
2024-06-26 10:20:45 +0300

    style fix

--- src/LandingPage-Components/ImageSection.js
+++ src/LandingPage-Components/ImageSection.js
@@ -10,7 +10,7 @@ const ImageSection = () => (
       <p className='image-p'>
         Welcome to a world where AI testing is not just a necessity but a catalyst for innovation! Our platform empowers individuals and organizations to validate and optimize AI-based systems with confidence and precision. Whether you're a seasoned professional or a curious newcomer, our intuitive tools and comprehensive testing methodologies are here to support your journey towards AI excellence.
       </p>
-      <a href="#top" className="image-start-button">Start Testing Today!</a>
+      <a href="#top" className="image-start-button btn-landingpage">Start Testing Today!</a>
     </div>
     <img src={think} alt="Empower AI Innovations" className="image-image" />
   </div>

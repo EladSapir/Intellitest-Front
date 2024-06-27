5b5bd3fd384aeae5c94019e3a0f1eb72b7bb39dd
Elad Sapir <elad.1999.1999@gmail.com>
2024-06-27 13:32:11 +0300

    buttons rolls down and up

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -5,30 +5,31 @@ import Features from '../LandingPage-Components/Features';
 import ImageSection from '../LandingPage-Components/ImageSection';
 import Footer from '../LandingPage-Components/Footer';
 import bgImg from '../Images/Page_bg.png';
-//import bgImg2 from '../Images/BG2.svg';
 import Contribute from '../ContributeSection/ContributeSection';
 import './LandingPage.css'
 
 function LandingPage({ onLogin }) {
     const [user, setUser] = useState(null);
-  
+
     const handleLogin = (userData) => {
-      setUser(userData);
-      onLogin(userData);
+        setUser(userData);
+        onLogin(userData);
     };
-  
+
     return (
-      <div className='lp'>
-        <Header user={user} />
-        <div className="content-container">
-          <MainSection onLogin={handleLogin} />
-          <Features />
-          <ImageSection />
-          <Contribute />
+        <div className='lp'>
+            <Header user={user} />
+            <div className="content-container">
+                <MainSection onLogin={handleLogin} />
+                <Features />
+                <ImageSection />
+                <div id="contribute">
+                    <Contribute />
+                </div>
+            </div>
+            <Footer />
         </div>
-        <Footer />
-      </div>
     );
-  }
-  
+}
+
 export default LandingPage;
--- src/LandingPage-Components/Header.js
+++ src/LandingPage-Components/Header.js
@@ -3,16 +3,16 @@ import logo from '../Images/logo.png';
 import './Header.css';
 
 const Header = () => (
-  <div className="header-container">
-    <div className="logo-container-dashboard">
-      <img className="logo" src={logo} alt="IntelliTest" />
-      <span className="logo-text-dashboard">IntelliTest</span>
+    <div className="header-container">
+        <div className="logo-container-dashboard">
+            <img className="logo" src={logo} alt="IntelliTest" />
+            <span className="logo-text-dashboard">IntelliTest</span>
+        </div>
+        <div className="nav-links">
+            <a href="#contribute">Contact Us</a>
+            <a href="#">Buy us a coffee</a>
+        </div>
     </div>
-    <div className="nav-links">
-      <a href="#">Contact Us</a>
-      <a href="#">Buy us a coffee</a>
-    </div>
-  </div>
 );
 
 export default Header;
